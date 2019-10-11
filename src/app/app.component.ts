83247e18d9d6c2b494b77594438de9964bdbb7ac
Luis Huertas <hu3rtas@gmail.com>
2019-10-10 21:43:08 -0500

    Start Point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-ab',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AltasBajasFormComponent } from './altas-bajas-formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AltasBajasFormComponent
   ],
   imports: [
     BrowserModule,
